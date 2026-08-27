d00d134442716a121403eb2b85874a915f93b6a8
moediabdev-design <moediabdev@gmail.com>
2026-08-27 23:14:28 +0200

    Create header-scroll.js

---
+++ header-scroll.js
@@ -0,0 +1,24 @@
+window.onload = function() {
+  var header = document.getElementById('site-header-desktop');
+  var mobileHeader = document.getElementById('site-header-mobile');
+  var lastY = window.pageYOffset;
+  var hidden = false;
+
+  setInterval(function() {
+    var y = window.pageYOffset;
+    var delta = y - lastY;
+
+    if (y <= 10) {
+      hidden = false;
+    } else if (delta > 5) {
+      hidden = true;
+    } else if (delta < -5) {
+      hidden = false;
+    }
+
+    if (header) header.style.top = hidden ? '-150px' : '0px';
+    if (mobileHeader) mobileHeader.style.top = hidden ? '-90px' : '0px';
+
+    if (Math.abs(delta) > 2) lastY = y;
+  }, 100);
+};
