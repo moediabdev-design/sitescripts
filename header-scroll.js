bd0fff966a2a0c3c1ee94a6b3e1df7f9ad06fb39
moediabdev-design <moediabdev@gmail.com>
2026-08-27 23:25:38 +0200

    Update header-scroll.js

--- header-scroll.js
+++ header-scroll.js
@@ -1,16 +1,12 @@
 window.onload = function() {
-  var debugBox = document.getElementById('debug-box');
-  var header = document.getElementById('site-header-desktop');
+  var desktopHeader = document.getElementById('site-header-desktop');
+  var mobileHeader = document.getElementById('site-header-mobile');
   var lastY = window.pageYOffset;
   var hidden = false;
 
   setInterval(function() {
     var y = window.pageYOffset;
     var delta = y - lastY;
-
-    if (debugBox) {
-      debugBox.textContent = 'y=' + y + ' delta=' + delta.toFixed(1) + ' hidden=' + hidden;
-    }
 
     if (y <= 10) {
       hidden = false;
@@ -20,12 +16,9 @@ window.onload = function() {
       hidden = false;
     }
 
-    if (header) {
-      header.style.top = hidden ? '-150px' : '0px';
-    }
+    if (desktopHeader) desktopHeader.style.top = hidden ? '-150px' : '0px';
+    if (mobileHeader) mobileHeader.style.top = hidden ? '-90px' : '0px';
 
-    if (Math.abs(delta) > 2) {
-      lastY = y;
-    }
+    if (Math.abs(delta) > 2) lastY = y;
   }, 100);
 };
